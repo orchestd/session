39107866237984d1b83bfee5836c1596db5ae139
andrey <andrey.m@heilasystems.com>
2024-02-26 14:41:37 +0200

    added cacheType filter to Freeze

--- session.go
+++ session.go
@@ -17,7 +17,7 @@ type SessionResolver interface {
 	NewSession(id string) Session
 	SaveSession(c context.Context, cSession Session) error
 	GetCurrentSession(c context.Context) (Session, error)
-	FreezeCacheVersionsForSession(c context.Context, curSession Session, action string) error
+	FreezeCacheVersionsForSession(c context.Context, curSession Session, action string, cacheType string) error
 	UnFreezeCacheVersionsForSession(c context.Context, curSession Session, action string) error
 	IsObsolete(c context.Context, sessionId string) (bool, error)
 }
@@ -60,6 +60,6 @@ type DeviceInfoResolver interface {
 type SessionRepo interface {
 	GetUserSessionByTokenToStruct(context context.Context, token string, dest interface{}) (bool, error)
 	InsertOrUpdate(ctx context.Context, id string, obj interface{}) error
-	GetCacheVersions(ctx context.Context, now time.Time, filterAction string) (map[string]string, error)
+	GetCacheVersions(ctx context.Context, now time.Time, filterAction string, filterType string) (map[string]string, error)
 	GetCollectionsFilterActions(ctx context.Context, filterAction string) ([]string, error)
 }
