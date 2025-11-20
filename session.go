79baa9062c5daaaf1819464a6d682e7f9b84a165
tamir <tamir.c@heilasystems.com>
2025-11-20 11:29:17 +0200

    POS-11202: move session hooker

--- session.go
+++ session.go
@@ -45,6 +45,7 @@ type Session interface {
 	GetDeviceInfo() DeviceInfoResolver
 	SetReferrer(string)
 	GetReferrer() string
+	ValidateLocal(ctx context.Context, i interface{}) error
 }
 
 type DeviceInfoResolver interface {
--- sessionresolver/wrapper.go
+++ sessionresolver/wrapper.go
@@ -19,24 +19,6 @@ type sessionWrapper struct {
 const DataVersionsKey = "versions"
 const DataNowKey = "dateNow"
 
-type SessionHooker interface {
-	ValidateLocal(c context.Context, session sessionWrapper) error
-}
-
-//func ValidateLocal(s session.Session, i interface{}) error {
-//	if validator, ok := i.(SessionHooker); ok {
-//		return validator.ValidateLocal(s)
-//	}
-//	return nil
-//}
-
-func (s sessionWrapper) ValidateLocal(c context.Context, i interface{}) error {
-	if validator, ok := i.(SessionHooker); ok {
-		return validator.ValidateLocal(c, s)
-	}
-	return nil
-}
-
 type ActiveOrder struct {
 	Id             string            `json:"id"`
 	SubServiceType string            `json:"subServiceType"`
@@ -230,6 +212,17 @@ func (c *currentSession) SetReferrer(referrer string) {
 
 func (c currentSession) GetReferrer() string {
 	return c.Referrer
+}
+
+func (cs *currentSession) ValidateLocal(c context.Context, i interface{}) error {
+	if validator, ok := i.(SessionHooker); ok {
+		return validator.ValidateSession(c, cs)
+	}
+	return nil
+}
+
+type SessionHooker interface {
+	ValidateSession(c context.Context, session session.Session) error
 }
 
 func (sw sessionWrapper) NewSession(id string) session.Session {
